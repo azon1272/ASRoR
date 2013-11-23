a935c34f957dc9fee1ffafb7ba9c69baa9904f72
azon1272 <azon1272@gmail.com>
2013-11-23 22:26:35 +0200

    Second commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131123174950) do
+ActiveRecord::Schema.define(version: 20131123185503) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
