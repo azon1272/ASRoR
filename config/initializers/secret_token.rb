d7256d9eb42b1c9a8fe0c692d112c6f50587708f
azon1272 <azon1272@gmail.com>
2013-11-23 20:28:46 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testpro::Application.config.secret_key_base = '28a5f80cb6476c6c2747c3386a76f6f5fa92fe1c8e89f45d40290d94c0f6f5e01c040626cc8e83ebc1872fffceed96a5a0863e73fb2d2391ba2b7752677b838c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testpro::Application.config.session_store :cookie_store, key: '_testpro_session'
